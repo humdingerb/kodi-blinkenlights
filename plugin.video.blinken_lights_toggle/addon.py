aeed73a747fe8a3956f436d554d79025c4074dd0
Humdinger <humdingerb@gmail.com>
2019-10-15 09:01:26 +0200

    v2.0 to be used with Raspian (systemd)

--- plugin.video.blinken_lights_toggle/addon.py
+++ plugin.video.blinken_lights_toggle/addon.py
@@ -3,14 +3,14 @@ import xbmcgui
 import os
 import commands
 
-output = commands.getoutput('ps -A | grep -v grep | grep -c hyperiond')
-		
-if output[0] == "0":
-        cmd     = "/storage/hyperion/bin/hyperiond.sh /storage/.config/hyperion.config.json </dev/null >/dev/null 2>&1 &"
-        line    = "Lights On"
-else:
-        cmd     = "killall hyperiond"
+output = commands.getoutput('systemctl is-active hyperion')
+
+if output == "active":
+        cmd     = "sudo systemctl stop hyperion"
         line    = "Lights Off"
+else:
+        cmd     = "sudo systemctl start hyperion"
+        line    = "Lights On"
 
 os.system(cmd)
 xbmc.executebuiltin('Notification(Hyperion,%s,2000)' % line)
